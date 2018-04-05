b971a39ffaa885bf358aeb926248a3f92e7248cb
evgeniakuzina <30548350+evgeniakuzina@users.noreply.github.com>
2018-04-05 10:03:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,27 +10,19 @@ $continents = [
     'Australia' => ['Macropus rufus', 'Phascolarctos cinereus', 'Sarcophilus harrisii', 'Dromaius'],
 ];
 
-//Select animals which names consist of two words
-
-$twoWordAnimals = [];
-
-foreach ($continents as $continent => $animals) {
-	foreach ($animals as $animal) {
-		$twoWordAnimal = explode(' ', $animal);
-		if (count($twoWordAnimal) > 1) { 
-			$twoWordAnimals[] = $twoWordAnimal;
-		}
-	}
-}
-
-// Create two separate arrays of first part of animals' name and second part of animals' names
+//Select animals which names consist of two words and create two separate arrays of first part of animals' name and second part of animals' names
 
 $firstWords = [];
 $secondWords = [];
 
-foreach ($twoWordAnimals as $value) {
-	$firstWords[] = $value[0];
-	$secondWords[] = $value[1];
+foreach ($continents as $continent => $animals) {
+	foreach ($animals as $animal) {
+		$twoWordAnimal = explode(' ', $animal);
+		if (count($twoWordAnimal) == 2) { 
+			$firstWords[] = $twoWordAnimal[0];
+			$secondWords[] = $twoWordAnimal[1];
+		}
+	}
 }
 
 //Shuffle names in two arrays and create new array of animals 
